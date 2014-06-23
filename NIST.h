45fc7b020579dc47205f080ac35ac7dd2395dc5c
Ventura Daniela <daniela1988@gmail.com>
2014-06-23 17:55:34 +0200

    Progress but problem with concatenation of arrays

--- NIST.h
+++ NIST.h
@@ -12,27 +12,29 @@ class NIST
 
 	private:
 		HMAC_type hmac_algorithm;
-		void (NIST::*init_prf) (uint8_t key[], int key_length);
-    	uint8_t* (NIST::*prf) (uint8_t data[], int data_length);
-    	void (NIST::*print_prf_result) (uint8_t* hash);
+		int prfOutputSizeBit; // = h for NIST specifics
+		void (NIST::*init_prf) (uint8_t* key, int key_length);
+    	uint8_t* (NIST::*prf) (uint8_t* data, int data_length);
 
-    	//init hmac-shaX function
+    	//init PRF function
     	void init_prf_function(void);
 
     	//sha-1 functions
-    	void init_hmacSha1PRF (uint8_t key[], int key_length);
-    	uint8_t* hmacSha1PRF (uint8_t data[], int data_length);
-    	void printSha1(uint8_t* hash);
+    	void init_hmacSha1PRF (uint8_t* key, int key_length);
+    	uint8_t* hmacSha1PRF (uint8_t* data, int data_length);
 
     	//sha-256 functions
-   		void init_hmacSha256PRF (uint8_t key[], int key_length);
-    	uint8_t* hmacSha256PRF(uint8_t data[], int data_length);
-		void printSha256(uint8_t* hash);
+   		void init_hmacSha256PRF (uint8_t* key, int key_length);
+    	uint8_t* hmacSha256PRF(uint8_t* data, int data_length);
+
+		//utilities
+		uint8_t* updateDataInput (uint8_t ctr, uint8_t* fixedInput, int fixedInput_lenght);
+		//void printBytesResulted(uint8_t* hash, int lenght);
 
 	public:
 		void initialize (HMAC_type algorithm_name);
 		uint8_t* KDFCounterMode(uint8_t* keyDerivationKey, int outputSizeBit, uint8_t* fixedInput, int keyDerivationKey_lenght, int fixedInput_lenght);
-    	
+    	void printBytesResulted(uint8_t* hash, int lenght);
 };
 
 #endif
